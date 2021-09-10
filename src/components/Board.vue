55a9e24d9660329399203dd7c3e3a5d15f3b3ad5
Muhammad Fuzail <mfuzail1991@gmail.com>
2021-09-11 02:22:38 +0700

    done

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="board">
-    <div v-for="(rows, indexRow) in inputs" :key="indexRow">
-      <input
-        min="1"
-        max="9"
-        type="number"
-        maxlength="1"
-        v-for="(col, indexCol) in rows"
-        :key="indexCol"
-        v-model="col.val"
-      />
+    <div v-if="!isSudokuValid">
+      <p>This sudoku is unsolvable!!!</p>
     </div>
-    <button @click="generateSolution">test</button>
+    <div class="content">
+      <div v-if="loading" class="loading">
+        <p>Please wait...</p>
+      </div>
+      <div class="container">
+        <div v-for="(rows, indexRow) in inputs" :key="indexRow">
+          <input
+            type="number"
+            v-for="(col, indexCol) in rows"
+            :key="indexCol"
+            :value="col.val"
+            @input="handleInput($event, indexRow, indexCol)"
+            :disabled="isButtonDisabled"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,21 +29,55 @@ export default {
   data() {
     return {
       inputs: this.generateArray(),
-      solution: [],
+      solution: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ],
       step: 0,
       totalStep: 0,
       isSudokuValid: true,
+      isButtonDisabled: false,
+      isSudokuDone: false,
+      loading: false,
+      time: 2000,
     };
   },
-  watch: {
-    step: function (val) {
-      console.log("watch:", val);
-    },
-  },
   methods: {
+    handleInput(e, x, y) {
+      // console.log(e.target.value.length);
+      if (e.target.value.length > 1) {
+        const value = e.target.value.substr(e.target.value.length - 1);
+        this.inputs[x][y].val = value;
+      }
+      // console.log(e.target.value, x, y);
+    },
     generateSolution() {
       this.solve(this.transformVal());
-      console.log(this.step);
+      this.loading = true;
+      setTimeout(() => {
+        this.inputs = this.solution.map((element) => {
+          let arr = [];
+          element.forEach((el) => {
+            arr.push({ val: el });
+          });
+          return arr;
+        });
+        this.loading = false;
+        this.isButtonDisabled = true;
+        this.isSudokuDone = true;
+      }, this.time);
+    },
+    reset() {
+      this.isButtonDisabled = false;
+      this.inputs = this.generateArray();
+      this.isSudokuValid = true;
     },
     generateArray() {
       const len = 9;
@@ -110,40 +152,33 @@ export default {
       let row = emptyVal[0];
       let col = emptyVal[1];
 
+      if (this.totalStep > 10000 || this.step < 0) {
+        setTimeout(() => {
+          this.isSudokuValid = false;
+        }, this.time);
+        return;
+      }
+
       if (row === -1) {
-        return board;
+        return;
       }
 
       for (let val = 1; val <= 9; val++) {
-        if (this.validateAll(row, col, board, val)) {
-          board[row][col] = val;
+        if (this.validateAll(row, col, this.solution, val)) {
+          // board[row][col] = val;
           this.solution[row][col] = val;
           // recursive solve
           this.step++;
           this.totalStep++;
-          this.solve(board);
+          this.solve(this.solution);
         }
       }
 
       // if not valid which means no solution, reset to zero
-      if (this.findEmptyValue(board)[0] !== -1) {
+      if (this.findEmptyValue(this.solution)[0] !== -1) {
         this.step--;
-        board[row][col] = 0;
+        // board[row][col] = 0;
         this.solution[row][col] = 0;
-      }
-
-      if (this.step <= 0) {
-        this.isSudokuValid = false;
-      }
-
-      return board;
-    },
-    sleep(milisecond) {
-      var start = new Date().getTime();
-      for (var i = 0; i < 1e7; i++) {
-        if (new Date().getTime() - start > milisecond) {
-          break;
-        }
       }
     },
   },
@@ -163,6 +198,33 @@ input {
   background-color: white;
   text-align: center;
   font-size: 1.2rem;
+}
+
+input[disabled] {
+  background-color: rgba(128, 128, 128, 0.411);
+}
+
+.content {
+  position: relative;
+  display: inline-block;
+}
+
+.container {
+  margin: auto;
+}
+.loading {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(99, 99, 99, 0.61);
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.loading p {
+  font-size: 2.2rem;
+  color: white;
 }
 
 @media only screen and (max-width: 600px) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <board></board>
+    <h2>SODOKU SOLVER</h2>
+    <board ref="board"></board>
+    <br />
+    <div>
+      <button @click="submit">SUBMIT</button>
+      <button @click="reset">RESET</button>
+    </div>
   </div>
 </template>
 
@@ -10,5 +16,43 @@ import Board from "../components/Board.vue";
 export default {
   name: "Home",
   components: { Board },
+  methods: {
+    submit() {
+      this.$refs.board.generateSolution();
+    },
+    reset() {
+      this.$refs.board.reset();
+    },
+  },
 };
 </script>
+
+<style lang="css" scoped>
+button {
+  position: relative;
+  cursor: pointer;
+  margin: 1rem;
+  padding: 5px 20px;
+  border: 1px black solid;
+  background-color: rgba(255, 255, 255, 0);
+  transition: all 0.3s ease-in-out;
+}
+button::before {
+  content: "";
+  position: absolute;
+  display: block;
+  height: 100%;
+  width: 100%;
+  transform: translate(-20px, -5px);
+  transition: all 0.3s ease-in-out;
+  z-index: -1;
+}
+
+button:hover::before {
+  background-color: black;
+}
+
+button:hover {
+  color: white;
+}
+</style>
